10375ed836fa4c894e7cb21dcc8f571114ea3201
Javier Beneito Barquero <jbbarquero@gmail.com>
2016-06-02 16:08:00 +0200

    Boot project for voting

---
+++ docker-birthday-3-boot-voting/src/main/java/com/malsolo/docker/birthday/voting/DockerBirthday3BootVotingApplication.java
@@ -0,0 +1,12 @@
+package com.malsolo.docker.birthday.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBirthday3BootVotingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBirthday3BootVotingApplication.class, args);
+	}
+}
